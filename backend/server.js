45bfa03e46c561cda87ba0519d9427833c53dcf6
Ashish-Techugo <ashish.bisht@techugo.com>
2022-10-18 18:33:45 +0530

    cors code

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const authRoute=require('./routes/authRoute');
+const cors=require('cors');
 require('dotenv').config()
 const app = express();
 const mongoose = require('mongoose')
 const MongoURL = process.env.MONGOB_URL;
 
 app.use(express.json())
+app.use(cors());
 app.use('/storage',express.static('storage'))
 mongoose.connect(MongoURL, {
     useNewUrlParser: true,
